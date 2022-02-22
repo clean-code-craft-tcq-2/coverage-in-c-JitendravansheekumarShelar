d1c755e225e6fe72941e1ba1059f7f1a9568e3d8
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-02-22 23:46:22 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#define PASSIVE_COOLING_LOWERLIMIT 0u
+#define PASSIVE_COOLING_UPPERLIMIT 35u
+#define HI_ACTIVE_COOLING_LOWERLIMIT 0u
+#define HI_ACTIVE_COOLING_UPPERLIMIT 45u
+#define MED_ACTIVE_COOLING_LOWERLIMIT 0u
+#define MED_ACTIVE_COOLING_UPPERLIMIT 40u
+
+#define NoOfCoolingType 3u
+#define NoOfBreachType 3u
+
+//enum declarations
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -7,31 +18,45 @@ typedef enum {
 } CoolingType;
 
 typedef enum {
-  NORMAL,
   TOO_LOW,
-  TOO_HIGH
+  TOO_HIGH,
+  NORMAL
 } BreachType;
-
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL
 } AlertTarget;
 
+//structure declarations
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+typedef struct
+{
+   CoolingType  CoolingType_en;
+   int          CoolingLowerLimit;
+   int          CoolingUpperLimit;
+}coolingTypeInfo_st;
 
-#define PASSIVE_COOLING_LOWERLIMIT 0u
-#define PASSIVE_COOLING_UPPERLIMIT 35u
-#define HI_ACTIVE_COOLING_LOWERLIMIT 0u
-#define HI_ACTIVE_COOLING_UPPERLIMIT 45u
-#define MED_ACTIVE_COOLING_LOWERLIMIT 0u
-#define MED_ACTIVE_COOLING_UPPERLIMIT 40u
+typedef struct
+{
+   CoolingType  CoolingType_en;
+   char*        emailMessage;
+}breachTypeInfo_st;
+
+//function declarations
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+int checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+int sendToController(BreachType breachType);
+int sendToEmail(BreachType breachType);
+int invokeAlertTarget(AlertTarget alertTarget,BreachType breachType);
+int print_for_LOW_HIGH_breach(BreachType breachType,const char* recepient);
+int sendMessageBasedOnbreachType(BreachType breachType,const char* recepient);
+
+//variable declarations
+extern const coolingTypeInfo_st coolingTypeInfo_pst[NoOfCoolingType];
+extern const breachTypeInfo_st breachTypeInfo_pst[NoOfBreachType];
